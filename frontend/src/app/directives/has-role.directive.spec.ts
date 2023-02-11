48a237554bea21be2cfc3a6fb4dc59020397b916
SJarno <jarno.saastamoinen@hotmail.com>
2023-02-11 22:06:38 +0200

    update action

--- frontend/src/app/directives/has-role.directive.spec.ts
+++ frontend/src/app/directives/has-role.directive.spec.ts
@@ -1,23 +1,44 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TemplateRef, ViewContainerRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { AuthResponse } from '../models/AuthResponse';
+import { Role } from '../models/Role';
 import { AuthService } from '../services/auth.service';
 import { HasRoleDirective } from './has-role.directive';
 
 describe('HasRoleDirective', () => {
   let authService: AuthService;
+  let viewContainerRef: ViewContainerRef;
+  let templateRef: TemplateRef<any>;
+  let directive: HasRoleDirective;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       providers: [AuthService]
     });
     authService = TestBed.inject(AuthService);
+    viewContainerRef = jasmine.createSpyObj<ViewContainerRef>('ViewContainerRef', ['clear', 'createEmbeddedView']);
+    templateRef = jasmine.createSpyObj<TemplateRef<any>>('TemplateRef', ['createEmbeddedView']);
+    directive = new HasRoleDirective(authService, viewContainerRef, templateRef);
   });
 
   it('should create an instance', () => {
-    const viewContainerRef = jasmine.createSpyObj<ViewContainerRef>('ViewContainerRef', ['clear', 'createEmbeddedView']);
-    const templateRef = jasmine.createSpyObj<TemplateRef<any>>('TemplateRef', ['createEmbeddedView']);
-    const directive = new HasRoleDirective(authService, viewContainerRef, templateRef);
     expect(directive).toBeTruthy();
+  });
+  it('should call viewContainerRef.clear and viewContainerRef.createEmbeddedView', () => {
+    
+    directive.hasRole = 'ADMIN';
+    const role = 'ADMIN';
+    directive.hasRole = role;
+    expect(directive['role']).toEqual(false);
+    
+  });
+  it('should check the role correctly', () => {
+    const role = 'ADMIN';
+    directive['user'] = {
+      roles: [{ authority: 'ADMIN' } as Role]
+    } as AuthResponse;
+    const result = directive['checkRole'](role);
+    expect(result).toEqual(true);
   });
 });
--- frontend/src/app/directives/is-authenticated.directive.spec.ts
+++ frontend/src/app/directives/is-authenticated.directive.spec.ts
@@ -1,26 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TemplateRef, ViewContainerRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { AuthResponse } from '../models/AuthResponse';
 import { AuthService } from '../services/auth.service';
 import { IsAuthenticatedDirective } from './is-authenticated.directive';
 
 describe('IsAuthenticatedDirective', () => {
   let directive: IsAuthenticatedDirective;
-    let authService: AuthService;
-    let viewContainerRef: ViewContainerRef;
-    let templateRef: TemplateRef<any>;
+  let authService: AuthService;
+  let viewContainerRef: ViewContainerRef;
+  let templateRef: TemplateRef<any>;
 
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientModule],
-        providers: [AuthService, ViewContainerRef, { provide: TemplateRef, useValue: {} }]
-      });
-      authService = TestBed.inject(AuthService);
-      viewContainerRef = TestBed.inject(ViewContainerRef);
-      templateRef = TestBed.inject(TemplateRef);
-      directive = new IsAuthenticatedDirective(templateRef, viewContainerRef, authService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AuthService, ViewContainerRef, { provide: TemplateRef, useValue: {} }]
     });
+    authService = TestBed.inject(AuthService);
+    viewContainerRef = jasmine.createSpyObj<ViewContainerRef>('ViewContainerRef', ['clear', 'createEmbeddedView']);
+    templateRef = jasmine.createSpyObj<TemplateRef<any>>('TemplateRef', ['createEmbeddedView']);
+    directive = new IsAuthenticatedDirective(templateRef, viewContainerRef, authService);
+  });
   it('should create an instance', () => {
     expect(directive).toBeTruthy();
   });
+  it('should call viewContainer.clear and viewContainer.createEmbeddedView', () => {
+    directive['user'] = { authenticated: true } as AuthResponse;
+    directive.isAuthenticated = true;
+
+    expect(viewContainerRef.clear).toHaveBeenCalledTimes(1);
+    //expect(templateRef.createEmbeddedView).toHaveBeenCalledTimes(1);
+  });
+
 });
